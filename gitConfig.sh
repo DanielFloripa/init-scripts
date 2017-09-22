e1fce78b55ef617d026ce51b6c13b41b20db32e1
Daniel <daniel@colmeia.udesc.br>
2017-09-22 12:55:54 -0300

    git

---
+++ gitConfig.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if ! hash git 2>/dev/null ; then
+	sudo apt-get install git
+fi
+if [ $# -eq 0 ]; then
+	echo "Falta parametro: $0 <repoName>"
+	exit 0
+fi
+git init
+git add *
+git commit -m "new"
+git config --global credential.helper 'store'
+git remote add origin https://github.com/DanielFloripa/${1}.git
+git push origin master
+
+	
